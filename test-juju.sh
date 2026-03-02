42250d98d174b2961b1a02f822e7ed22e691c7c3
Marian Gasparovic <marian.gasparovic@canonical.com>
2026-03-02 10:26:15 +0100

    another change to test

--- test-juju.sh
+++ test-juju.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash 
 
 # Get the number of juju nodes from terraform
 JUJU_NODE_COUNT=$(cd units/virtualnodes && terragrunt output -json juju_nodes | jq 'length')
@@ -11,7 +11,12 @@ juju controllers --refresh
 juju add-model test
 juju deploy --force --channel 16/edge  -n 3 postgresql
 juju deploy -n 3 ubuntu
-sleep 600
+echo "sleep for 60 seconds"
+sleep 60
+juju status
+juju status -m controller
+echo "sleep for 20 minutes"
+sleep 1200
 juju status
 juju status -m controller
 for i in {1..20} ; do for j in 0 1 2; do juju exec --unit ubuntu/$j date ; done ; done
